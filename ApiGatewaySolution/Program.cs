0c1ea083d30ef35826d0aa6e3a209f154f856a55
Lotta Harmonen <lotta.harmonen@gmail.com>
2024-12-12 10:56:57 +0100

    ocelot file updates to match the right endpoints

--- ApiGatewaySolution/Program.cs
+++ ApiGatewaySolution/Program.cs
@@ -5,17 +5,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
+builder.Services.AddLogging(logging =>
+{
+    logging.ClearProviders();  
+    logging.AddConsole();     
+    logging.SetMinimumLevel(LogLevel.Debug);  
+});
+
 builder.Services.AddOcelot(builder.Configuration);
 
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
-
-app.UseSwagger();
-app.UseSwaggerUI();
 
 await app.UseOcelot();
 
